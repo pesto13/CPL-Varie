ed6bdeb82294eddb0b5e7490fcedfcee56c6c242
pesto13 <ricola1304@gmail.com>
2024-04-04 12:01:38 +0200

    scrivo xml

--- XMLNextcloud/src/XmlManager.py
+++ XMLNextcloud/src/XmlManager.py
@@ -1,17 +1,21 @@
 import xml.etree.ElementTree as ET
 
-XMLFILE = 'CasisticaDate.xls'
+class XmlManager:
+    def __init__(self, filename):
+        self.tree = ET.parse(filename)
+        self.root = self.tree.getroot()
 
-class ExcelManager:
-
-    @staticmethod
-    def _read_XML(filename=XMLFILE):
-        pass
-
-    def __init__(self) -> None:
-        # possibile passaggio valore del file
-        self.data = ExcelManager._read_XML()
-
-    
-
+    def check_and_update_data(self, parent_tag_name, first_tag_name, second_tag_name):
+        for parent_tag in self.root.findall(parent_tag_name):
+            tag1 = parent_tag.find('.//' + first_tag_name)
+            tag2 = parent_tag.find('.//' + second_tag_name)
+            print(tag1.text)
+            tag1.text = 'lol'
+            tag2.text = 'ciao'
+            print(tag1.text)
         
+        self.tree.write('questo2.xml')
+
+# Utilizzo della classe XmlManager
+xml_manager = XmlManager('questo.xml')
+xml_manager.check_and_update_data('DatiPdr', 'pros_fin_inizio', 'pros_fin_fine')
