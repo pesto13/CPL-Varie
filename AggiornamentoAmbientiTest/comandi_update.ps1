d16ec350889bfff0302a7bba10fc5376230e965a
pesto13 <ricola1304@gmail.com>
2024-06-11 11:39:00 +0200

    aggiunto amministratore

--- AggiornamentoAmbientiTest/comandi_update.ps1
+++ AggiornamentoAmbientiTest/comandi_update.ps1
@@ -9,6 +9,10 @@ soresina -> SORESINATESTING
 New-NAVServerUser -FullName 'Francesco Bellodi' -ServerInstance 'bc210'-WindowsAccount 'CPGNET\bellodifrancesco'-LicenseType 'Full' -State 'Disabled'
 New-NAVServerUserPermissionSet -ServerInstance 'bc210' -PermissionSetId 'SUPER' -WindowsAccount 'CPGNET\bellodifrancesco'
 
+
+New-NAVServerUser -FullName 'Francesco Bellodi' -ServerInstance 'bc210'-WindowsAccount 'CPGNET\bellodif'-LicenseType 'Full' -State 'Disabled'
+New-NAVServerUserPermissionSet -ServerInstance 'bc210' -PermissionSetId 'SUPER' -WindowsAccount 'CPGNET\bellodif'
+
 # ti devi attivare
 
 Restart-NAVServerInstance 'bc210'
