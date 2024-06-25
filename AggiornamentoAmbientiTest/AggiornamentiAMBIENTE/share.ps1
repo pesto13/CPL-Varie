5d78248b4c4e152f5c552f6169b39dc49aab3ad4
pesto13 <ricola1304@gmail.com>
2024-06-25 17:11:50 +0200

    aggiornamento produzione automatico

---
+++ AggiornamentoAmbientiTest/AggiornamentiAMBIENTE/share.ps1
@@ -0,0 +1,15 @@
+param(
+    [string]$Filename
+)
+
+
+$credential = Get-Credential
+$pcProduzione = $pcProduzione = Get-Content -Path 'allProdPc.json' -Raw | ConvertFrom-Json
+$fileDestination = "C:\apps\DiNetwork"
+
+foreach ($pc in $pcProduzione){
+    $Session = New-PSSession -ComputerName $($pc.ComputerName) -Credential $credential
+    Copy-Item $Filename -Destination $fileDestination -ToSession $Session -Recurse -Force
+}
+
+# Remove-PSSession
